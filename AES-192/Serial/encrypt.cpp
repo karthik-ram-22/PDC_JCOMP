bc3860eecd74c9d55d2af0052becc6024266cccd
karthik-ram-22 <114208155+karthik-ram-22@users.noreply.github.com>
2022-11-12 19:33:30 +0530

    Add files via upload

---
+++ AES-192/Serial/encrypt.cpp
@@ -0,0 +1,230 @@
+
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include "structures.h"
+#include<sys/time.h>
+using namespace std;
+
+/* Serves as the initial round during encryption
+ * AddRoundKey is simply an XOR of a 128-bit block with the 192-bit key.
+ */
+void AddRoundKey(unsigned char * state, unsigned char * roundKey) {
+	for (int i = 0; i < 24; i++) {
+		state[i] ^= roundKey[i];
+	}
+}
+
+/* Perform substitution to each of the 24 bytes
+ * Uses S-box as lookup table 
+ */
+void SubBytes(unsigned char * state) {
+	for (int i = 0; i < 24; i++) {
+		state[i] = s[state[i]];
+	}
+}
+
+// Shift left, adds diffusion
+void ShiftRows(unsigned char * state) {
+	unsigned char tmp[24];
+
+	/* Column 1 */
+	tmp[0] = state[0];
+	tmp[1] = state[5];
+	tmp[2] = state[10];
+	tmp[3] = state[15];
+	
+	/* Column 2 */
+	tmp[4] = state[4];
+	tmp[5] = state[9];
+	tmp[6] = state[14];
+	tmp[7] = state[3];
+
+	/* Column 3 */
+	tmp[8] = state[8];
+	tmp[9] = state[13];
+	tmp[10] = state[2];
+	tmp[11] = state[7];
+	
+	/* Column 4 */
+	tmp[12] = state[12];
+	tmp[13] = state[1];
+	tmp[14] = state[6];
+	tmp[15] = state[11];
+
+	for (int i = 0; i < 24; i++) {
+		state[i] = tmp[i];
+	}
+}
+
+ /* MixColumns uses mul2, mul3 look-up tables
+  * Source of diffusion
+  */
+void MixColumns(unsigned char * state) {
+	unsigned char tmp[24];
+
+	tmp[0] = (unsigned char) mul2[state[0]] ^ mul3[state[1]] ^ state[2] ^ state[3];
+	tmp[1] = (unsigned char) state[0] ^ mul2[state[1]] ^ mul3[state[2]] ^ state[3];
+	tmp[2] = (unsigned char) state[0] ^ state[1] ^ mul2[state[2]] ^ mul3[state[3]];
+	tmp[3] = (unsigned char) mul3[state[0]] ^ state[1] ^ state[2] ^ mul2[state[3]];
+
+	tmp[4] = (unsigned char)mul2[state[4]] ^ mul3[state[5]] ^ state[6] ^ state[7];
+	tmp[5] = (unsigned char)state[4] ^ mul2[state[5]] ^ mul3[state[6]] ^ state[7];
+	tmp[6] = (unsigned char)state[4] ^ state[5] ^ mul2[state[6]] ^ mul3[state[7]];
+	tmp[7] = (unsigned char)mul3[state[4]] ^ state[5] ^ state[6] ^ mul2[state[7]];
+
+	tmp[8] = (unsigned char)mul2[state[8]] ^ mul3[state[9]] ^ state[10] ^ state[11];
+	tmp[9] = (unsigned char)state[8] ^ mul2[state[9]] ^ mul3[state[10]] ^ state[11];
+	tmp[10] = (unsigned char)state[8] ^ state[9] ^ mul2[state[10]] ^ mul3[state[11]];
+	tmp[11] = (unsigned char)mul3[state[8]] ^ state[9] ^ state[10] ^ mul2[state[11]];
+
+	tmp[12] = (unsigned char)mul2[state[12]] ^ mul3[state[13]] ^ state[14] ^ state[15];
+	tmp[13] = (unsigned char)state[12] ^ mul2[state[13]] ^ mul3[state[14]] ^ state[15];
+	tmp[14] = (unsigned char)state[12] ^ state[13] ^ mul2[state[14]] ^ mul3[state[15]];
+	tmp[15] = (unsigned char)mul3[state[12]] ^ state[13] ^ state[14] ^ mul2[state[15]];
+
+	for (int i = 0; i < 24; i++) {
+		state[i] = tmp[i];
+	}
+}
+
+/* Each round operates on 128 bits at a time
+ * The number of rounds is defined in AESEncrypt()
+ */
+void Round(unsigned char * state, unsigned char * key) {
+	SubBytes(state);
+	ShiftRows(state);
+	MixColumns(state);
+	AddRoundKey(state, key);
+}
+
+ // Same as Round() except it doesn't mix columns
+void FinalRound(unsigned char * state, unsigned char * key) {
+	SubBytes(state);
+	ShiftRows(state);
+	AddRoundKey(state, key);
+}
+
+/* The AES encryption function
+ * Organizes the confusion and diffusion steps into one function
+ */
+void AESEncrypt(unsigned char * message, unsigned char * expandedKey, unsigned char * encryptedMessage) {
+	unsigned char state[24]; // Stores the first 24 bytes of original message
+
+	for (int i = 0; i < 24; i++) {
+		state[i] = message[i];
+	}
+
+	int numberOfRounds = 10;//leaving intital and final (12-1-1=10)
+
+	AddRoundKey(state, expandedKey); // Initial round
+
+	for (int i = 0; i < numberOfRounds; i++) {
+		Round(state, expandedKey + (24 * (i+1)));
+	}
+
+	FinalRound(state, expandedKey + 240);
+
+	// Copy encrypted state to buffer
+	for (int i = 0; i < 24; i++) {
+		encryptedMessage[i] = state[i];
+	}
+}
+
+int main() {
+	struct timeval begin, end;
+    	gettimeofday(&begin, 0);
+
+	cout << "=============================" << endl;
+	cout << " 192-bit AES Encryption Tool   " << endl;
+	cout << "=============================" << endl;
+
+	char message[1024];
+
+	cout << "Enter the message to encrypt: ";
+	cin.getline(message, sizeof(message));
+	cout << message << endl;
+
+	// Pad message to 24 bytes
+	int originalLen = strlen((const char *)message);
+
+	int paddedMessageLen = originalLen;
+
+	if ((paddedMessageLen % 24) != 0) {
+		paddedMessageLen = (paddedMessageLen / 24 + 1) * 24;
+	}
+
+	unsigned char * paddedMessage = new unsigned char[paddedMessageLen];
+	for (int i = 0; i < paddedMessageLen; i++) {
+		if (i >= originalLen) {
+			paddedMessage[i] = 0;
+		}
+		else {
+			paddedMessage[i] = message[i];
+		}
+	}
+
+	unsigned char * encryptedMessage = new unsigned char[paddedMessageLen];
+
+	string str;
+	ifstream infile;
+	infile.open("keyfile", ios::in | ios::binary);
+
+	if (infile.is_open())
+	{
+		getline(infile, str); // The first line of file should be the key
+		infile.close();
+	}
+
+	else cout << "Unable to open file";
+
+	istringstream hex_chars_stream(str);
+	unsigned char key[24];
+	int i = 0;
+	unsigned int c;
+	while (hex_chars_stream >> hex >> c)
+	{
+		key[i] = c;
+		i++;
+	}
+
+	unsigned char expandedKey[176];
+
+	KeyExpansion(key, expandedKey);
+
+	for (int i = 0; i < paddedMessageLen; i += 24) {
+		AESEncrypt(paddedMessage+i, expandedKey, encryptedMessage+i);
+	}
+
+	cout << "Encrypted message in hex:" << endl;
+	for (int i = 0; i < paddedMessageLen; i++) {
+		cout << hex << (int) encryptedMessage[i];
+		cout << " ";
+	}
+
+	cout << endl;
+
+	// Write the encrypted string out to file "message.aes"
+	ofstream outfile;
+	outfile.open("message.aes", ios::out | ios::binary);
+	if (outfile.is_open())
+	{
+		outfile << encryptedMessage;
+		outfile.close();
+		cout << "Wrote encrypted message to file message.aes" << endl;
+	}
+
+	else cout << "Unable to open file";
+
+	// Free memory
+	delete[] paddedMessage;
+	delete[] encryptedMessage;
+	gettimeofday(&end, 0);
+    	long seconds = end.tv_sec - begin.tv_sec;
+    	long microseconds = end.tv_usec - begin.tv_usec;
+    	double elapsed = seconds + microseconds*1e-6;
+	printf("Time measured: %.3f seconds.\n", elapsed);
+
+	return 0;
+}
