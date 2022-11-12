626ab19afac1fec6d3a52689ab0c7055f30942a3
karthik-ram-22 <114208155+karthik-ram-22@users.noreply.github.com>
2022-11-12 19:26:11 +0530

    Add files via upload

---
+++ AES-192/Parallel/aes_types.hpp
@@ -0,0 +1,14 @@
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <string>
+
+// Types Byte (8 bits), Word (16 bits) and DWord (32 bits)
+
+typedef unsigned short int word;
+typedef unsigned long dword;
+
+string msg_final_mark;
+
+#endif
